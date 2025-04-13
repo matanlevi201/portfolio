7eb20191e7d768bdee9c8c83009c15ee31261395
Matan <ml.git201@gmail.com>
2025-04-13 21:03:02 +0300

    fix(build): fixed build bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/navbar";
 import ProjectSection from "./components/project-section";
-import TopProject from "./components/top-project";
+// import TopProject from "./components/top-project";
 import HeroSection from "./components/hero-section";
 // import ShowcaseProject from "./components/showcase-project";
 import { ScrollAnimation } from "./components/scroll-animation";
